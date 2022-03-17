3ac8776edeaad774a781dab4a524c451764d1d99
Choi Chang Hwan <cghy4862@gmail.com>
2022-03-17 16:31:05 +0900

    [RENAME] models.py class이름 Name -> User

--- students/changhwan/users/models.py
+++ students/changhwan/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Name(models.Model):
+class User(models.Model):
     name        = models.CharField(max_length=30)
     email       = models.EmailField(max_length=50)
     password    = models.CharField(max_length=30)
     phonenumber = models.CharField(max_length=20)
 
     class Meta:
-        db_table = 'names'
+        db_table = 'users'
