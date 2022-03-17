fb165c3db814af6ab63b6e387d9c2e759e96a244
Wonbin Lee <lob3767@gmail.com>
2022-03-17 20:04:00 +0900

    [Modict] app(users), Uesr model [updated_at]

--- students/wonbin/users/models.py
+++ students/wonbin/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
     created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta: 
         db_table = "users"
