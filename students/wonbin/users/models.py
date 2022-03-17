e13b7d4416642a5a837519365ce783001da7eb41
Wonbin Lee <lob3767@gmail.com>
2022-03-17 16:48:05 +0900

    [Modict] app(users), models migrate

--- students/wonbin/users/models.py
+++ students/wonbin/users/models.py
@@ -1,13 +1,13 @@
+from time import timezone
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length=20)
-    email        = models.EmailField(max_length=245)
-    password     = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    name         = models.CharField(max_length=100)
+    email        = models.EmailField(max_length=245, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    created_at   = models.DateField(auto_now_add=True)
+    updated_at   = models.DateField(auto_now_add=True)
 
     class Meta: 
         db_table = "users"
-  
-    
-
