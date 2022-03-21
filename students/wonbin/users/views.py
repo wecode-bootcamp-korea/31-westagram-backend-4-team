409b942a474791a98d1d123fa196b8967b8fe3ec
Wonbin Lee <lob3767@gmail.com>
2022-03-21 15:46:18 +0900

    [Add] users.views.py(SigninView)

--- students/wonbin/users/views.py
+++ students/wonbin/users/views.py
@@ -3,6 +3,7 @@ import json
 from django.db import IntegrityError
 from django.http import JsonResponse
 from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import User
 from .validator import email_validate, password_validate, phone_number_validate
@@ -31,3 +32,24 @@ class SignupView(View):
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
         except IntegrityError:
             return JsonResponse({"message":"This email already exists.."}, status=400)
+
+class SigninView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data["email"]
+            password = data["password"]
+
+            email_validate(email)
+            password_validate(password)
+            
+            user_email = User.objects.get(email = email)
+            if user_email.password == password:
+                return JsonResponse({"message": "success"}, status = 200)
+            else:
+                return JsonResponse({"message": "INVALID_USER"}, status = 401)
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+        except ObjectDoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status = 401)
