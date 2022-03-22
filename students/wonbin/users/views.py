91439ca6c3ed530f40f88af33ac6bc108f553d8c
Wonbin Lee <lob3767@gmail.com>
2022-03-22 14:48:31 +0900

    [Modict] users.views.py(SigninView)

--- students/wonbin/users/views.py
+++ students/wonbin/users/views.py
@@ -42,12 +42,10 @@ class SigninView(View):
             email_validate(email)
             password_validate(password)
              
-            if User.objects.get(email = email).password != password:
+            if not User.objects.filter(email = email, password = password).exists():
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)
                 
             return JsonResponse({"message": "success"}, status = 200)
                 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
-        except User.DoesNotExist:
-            return JsonResponse({"message": "INVALID_USER"}, status = 401)
